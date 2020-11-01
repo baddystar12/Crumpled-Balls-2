a603acfa1348b972991b9ed7103611c4ea69700c
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-10-31 17:38:50 -0700

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,24 @@
+class BaseClass{
+    constructor(x,y,width,height){
+        var options = {
+            friction : 0.5,
+            density : 1.2,
+            restitution: 1 
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width;
+        this.height=height;
+        this.image=loadImage("sprites/base.png")
+        World.add(world,this.body)
+    }
+    display(){
+        var pos =this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        imageMode(CENTER)
+        image(this.image,0, 0, this.width, this.height);
+        pop();
+      }   
+    }
+
+f
---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+
+var paper;
+var ground;
+var dustbin;
+
+function preload(){
+paper = loadImage("Images/paper.png");
+dustbin = loadImage("Images/dustbin.png")
+}
+
+	
+
+
+function setup() {
+	createCanvas(1200, 400);
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(100, 390, 40);
+	ground = new Ground(600, height, 1200, 20);
+	dustbin = new Dustbin(1100, 390, 100,10);
+	dustbin2 = new Dustbin(1050,380, 10, 100);
+	dustbin3 = new Dustbin(1150, 380, 10, 100);
+
+
+	
+  
+}
+
+
+function draw() {
+  background("white");
+  rectMode(CENTER);
+  Engine.update(engine);
+
+  paper1.display();
+  ground.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper1.body, paper1.body.position,{x:85,y:-85});
+	}
+}
+
