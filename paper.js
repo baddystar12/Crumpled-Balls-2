7476685137890c14709838e2e703de41d98e5092
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-02 06:39:20 -0800

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,6 +1,21 @@
-class Paper extends BaseClass{
-    constructor(x, y){
-        super(x,y,50,50)
-        this.image = loadImage("Images/paper.png");
+class Paper {
+    constructor(x, y, radius){
+        var paper_options = {
+            friction : 0.5,
+            density : 2.1,
+            restitution: 0.58
         }
+        this.image = loadImage("Images/paper.png");
+        this.radius = radius;
+        this.body = Matter.Bodies.circle(x,y,this.radius/2, paper_options);
+        World.add(world, this.body);
+    }
+    display(){
+        var position = this.body.position;
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(position.x, position.y, this.radius, this.radius);
+       /* imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);*/
+    }
 }
