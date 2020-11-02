dd364ee611ff7e127d144684b49e588196400520
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-11-02 06:41:01 -0800

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,6 +1,25 @@
-class Dustbin extends BaseClass {
+class Dustbin {
     constructor(x, y, width, height){
-    super(x,y,width,height);
-    this.image = loadImage("Images/dustbin.png");
+        var dustbin_options ={
+            friction:2,
+            density:1,
+            isStatic:true
+        }
+        this.image = loadImage("Images/dustbin.png");
+        this.body = Bodies.rectangle(x,y,width,height, dustbin_options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+    display(){
+        var position = this.body.position;
+        push();
+        translate(position.x, position.y);
+        rectMode(CENTER);
+        fill("white");
+        rect(0,0, this.width, this.height);
+       /* imageMode(CENTER);
+        this.image(this.image,1100,390,this.width,this.height);*/
+        pop();
     }
 }
